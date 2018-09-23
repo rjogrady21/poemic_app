d1eb4489693923b0dc50374c081c363aebe02f2a
Rich Ogrady <rjogrady21@gmail.com>
2018-09-23 10:15:12 -0700

    correction to poem

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   after_create :send_welcome_email
 
-  def send_daily_email
+  def send_daily_poem
     User.all.each do |user|
       YourMailer.daily_poem(user).deliver_now
     end
