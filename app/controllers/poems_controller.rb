86c14df2f1e1ffd0a7e4512c608f875cd9c87d76
Rich Ogrady <rjogrady21@gmail.com>
2018-10-14 19:54:46 +0200

    Allowing the show of the poem on the app

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,9 +1,19 @@
 class PoemsController < ApplicationController
+  before_action :set_poem, only: [:show]
     def self.send_daily_poem
       @users = User.all
       @poem = Poem.all.sample
       @users.each do |user|
         UserMailer.daily_poem(user, @poem).deliver_now
       end
+    end
+
+    def show
+    end
+
+    private
+
+    def set_poem
+      @poem = Poem.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact', as: :contact
   devise_for :users
   root to: 'pages#home'
+  get "poems/:id", to: "poems#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
