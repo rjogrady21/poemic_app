167aab9994eb6b17fc0082c5a0897a446ee66998
Richard O'Grady <rjogrady21@gmail.com>
2018-10-27 17:42:33 +0200

    Now connects the emailed poem to the poem in App

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact', as: :contact
   devise_for :users
   root to: 'pages#home'
-  get "poems/:id", to: "poems#show"
+  get "poems/:id", to: "poems#show",  as: :poem
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
